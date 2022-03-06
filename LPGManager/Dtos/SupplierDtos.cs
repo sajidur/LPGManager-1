6c6a813ed8be673017369207b6523a1a36e88ea3
Rafiq230 <rofiqul230@gmail.com>
2022-03-06 19:41:56 +0600

    remove unused code and update supplier model

---
+++ LPGManager/Dtos/SupplierDtos.cs
@@ -0,0 +1,12 @@
+﻿namespace LPGManager.Dtos
+{
+    public class SupplierDtos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Image { get; set; }
+        public string? Address { get; set; }
+        public int? Phone { get; set; }
+        public string? Companytype { get; set; }
+    }
+}
--- LPGManager/Models/Supplier.cs
+++ LPGManager/Models/Supplier.cs
@@ -2,10 +2,13 @@
 {
     public class Supplier
     {
-        public int SupplierId { get; set; }
+        public int SupplierId { get; set; }        
         public string SupplierName { get; set; }
+        public string? Image { get; set; }
         public string? Address { get; set; }
         public int? Phone { get; set; }
+        public string? Companytype { get; set; }
+        public DateTime CreatedOn { get; set; }
 
     }
 }
