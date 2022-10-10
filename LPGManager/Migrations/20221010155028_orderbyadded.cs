e3cc2684b3395e8a3f551217318bbb2bcaade54a
Md Sajidur rahman <sajid.ict@gmail.com>
2022-10-10 21:50:52 +0600

    order by receive by

---
+++ LPGManager/Migrations/20221010155028_orderbyadded.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LPGManager.Migrations
+{
+    public partial class orderbyadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                schema: "public",
+                table: "SellMasters");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                schema: "public",
+                table: "PurchaseMasters");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderBy",
+                schema: "public",
+                table: "SellMasters",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReceiveBy",
+                schema: "public",
+                table: "SellMasters",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OrderBy",
+                schema: "public",
+                table: "PurchaseMasters",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReceiveBy",
+                schema: "public",
+                table: "PurchaseMasters",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderBy",
+                schema: "public",
+                table: "SellMasters");
+
+            migrationBuilder.DropColumn(
+                name: "ReceiveBy",
+                schema: "public",
+                table: "SellMasters");
+
+            migrationBuilder.DropColumn(
+                name: "OrderBy",
+                schema: "public",
+                table: "PurchaseMasters");
+
+            migrationBuilder.DropColumn(
+                name: "ReceiveBy",
+                schema: "public",
+                table: "PurchaseMasters");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                schema: "public",
+                table: "SellMasters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                schema: "public",
+                table: "PurchaseMasters",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
